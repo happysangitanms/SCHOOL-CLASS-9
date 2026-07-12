a9cc3d250b7c4029fc7f579d1fde584aa3545ecf
happysangitanms <happysangitanms@gmail.com>
2026-07-12 17:28:42 +0530

    Update content.js

--- BHAUTIKA_BIGYANA/CLASS-2/content.js
+++ BHAUTIKA_BIGYANA/CLASS-2/content.js
@@ -3,7 +3,7 @@ const CLASS_MODE = "practice";
 const APP_CONFIG = {
     sreni: "ନବମ",
     subject: "ଭୌତିକ ବିଜ୍ଞାନ",
-    classCode: "ଭୌତିକ ବିଜ୍ଞାନ: ପୃଷ୍ଠା ୧-୨"
+    classCode: "ଭୌତିକ ବିଜ୍ଞାନ: ପ୍ରଥମ ଅଧ୍ୟାୟ (ପୃଷ୍ଠା ୧-୨)"
 };
 
 
