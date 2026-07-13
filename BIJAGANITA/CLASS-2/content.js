162e82035f7ca5c5d15d94dabc70cdc5ddb7e25c
happysangitanms <happysangitanms@gmail.com>
2026-07-13 17:21:43 +0530

    Update content.js

--- BIJAGANITA/CLASS-2/content.js
+++ BIJAGANITA/CLASS-2/content.js
@@ -3,14 +3,14 @@ const CLASS_MODE = "practice";
 const APP_CONFIG = {
     sreni: "ନବମ",
     subject: "ବୀଜଗଣିତ",
-    classCode: "ବୀଜଗଣିତ: ପ୍ରଥମ ଅଧ୍ୟାୟ (ପୃଷ୍ଠା 1-2)"
+    classCode: "ବୀଜଗଣିତ: ପ୍ରଥମ ଅଧ୍ୟାୟ (ପୃଷ୍ଠା 1-3)"
 };
 
 
 const CLASS_NAME = "school9";
 
 const MEDIA_CONFIG = {
-    summaryPdf: "https://audio.evidya.de/S9MTA1P1-2S.pdf"
+    summaryPdf: "https://audio.evidya.de/S9MTA1P1-3S.pdf"
 };
 
 
