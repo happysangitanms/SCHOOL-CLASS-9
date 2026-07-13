17e3c90872abc85fb5126fdf5b793dfd7b65a402
happysangitanms <happysangitanms@gmail.com>
2026-07-14 00:25:37 +0530

    Update content.js

--- SAHITYA_DHARA/CLASS-2/content.js
+++ SAHITYA_DHARA/CLASS-2/content.js
@@ -3,14 +3,14 @@ const CLASS_MODE = "practice";
 const APP_CONFIG = {
     sreni: "ନବମ",
     subject: "ସାହିତ୍ୟ ଧାରା",
-    classCode: "ସାହିତ୍ୟ ଧାରା : ପ୍ରଥମ ଅଧ୍ୟାୟ (ପୃଷ୍ଠା 1-2)"
+    classCode: "ସାହିତ୍ୟ ଧାରା : ପ୍ରଥମ ଅଧ୍ୟାୟ (ପୃଷ୍ଠା 1-3)"
 };
 
 
 const CLASS_NAME = "school9";
 
 const MEDIA_CONFIG = {
-    summaryPdf: "https://audio.evidya.de/S9SCP1P1-2S.pdf"
+    summaryPdf: "https://audio.evidya.de/S9FLO1P1-3S.pdf"
 };
 
 
