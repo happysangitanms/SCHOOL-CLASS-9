05bc9f9b208bc779792e10c63fdd197e28b25e6c
happysangitanms <happysangitanms@gmail.com>
2026-07-13 22:34:32 +0530

    Update content.js

--- JIBA_BIGYANA/CLASS-1/content.js
+++ JIBA_BIGYANA/CLASS-1/content.js
@@ -1,15 +1,15 @@
 const APP_CONFIG = {
     sreni: "ନବମ",
-    subject: "ଭୌତିକ ବିଜ୍ଞାନ"
+     subject: "ଜୀବ ବିଜ୍ଞାନ"
 };
 
 
 const CLASS_NAME = "school9";
 
 const MEDIA_CONFIG = {
-    audio1: "https://audio.evidya.de/S9SCP1P1-2.mp3",
+    audio1: "https://audio.evidya.de/S9SCL1P1-2.mp3",
     video:  "https://www.youtube.com/embed/jyNYTTROtU4?playsinline=1&rel=0&modestbranding=1&autoplay=1",
-    pdf:    "https://audio.evidya.de/S9SCP1P1-2.pdf"
+    pdf:    "https://audio.evidya.de/S9SCL1P1-2.pdf"
 };
 
 const wordData = {
