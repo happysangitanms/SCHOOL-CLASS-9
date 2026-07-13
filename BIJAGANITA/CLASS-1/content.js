e1b1f5afea3b375830b5c63a80891def3142d4f8
happysangitanms <happysangitanms@gmail.com>
2026-07-13 17:39:53 +0530

    Update content.js

--- BIJAGANITA/CLASS-1/content.js
+++ BIJAGANITA/CLASS-1/content.js
@@ -8,7 +8,7 @@ const CLASS_NAME = "school9";
 
 const MEDIA_CONFIG = {
     audio1: "https://audio.evidya.de/S9MTA1P1-3.mp3",
-    video:  "https://www.youtube.com/embed/MC5-QFQDrF?playsinline=1&rel=0&modestbranding=1&autoplay=1",
+    video:  "https://www.youtube.com/embed/uFXf9bHyiEM?playsinline=1&rel=0&modestbranding=1&autoplay=1",
     pdf:    "https://audio.evidya.de/S9MTA1P1-3.pdf"
 };
 
