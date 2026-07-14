99075f879e72f86e13a61ea6fb73eee766a1ae59
happysangitanms <happysangitanms@gmail.com>
2026-07-14 09:12:53 +0530

    Update content.js

--- SANSKRUTA/CLASS-1/content.js
+++ SANSKRUTA/CLASS-1/content.js
@@ -8,7 +8,7 @@ const CLASS_NAME = "school9";
 
 const MEDIA_CONFIG = {
     audio1: "https://audio.evidya.de/S9TLS1P2-3.mp3",
-    video:  "https://www.youtube.com/embed/jyNYTTROtU4?playsinline=1&rel=0&modestbranding=1&autoplay=1",
+    video:  "https://www.youtube.com/embed/QS0X7cpzo1g?playsinline=1&rel=0&modestbranding=1&autoplay=1",
     pdf:    "https://audio.evidya.de/S9TLS1P2-3.pdf"
 };
 
