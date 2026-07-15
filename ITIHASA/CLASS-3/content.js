b30ba8760297cd531dbe15ec81c873f5c4994d44
happysangitanms <happysangitanms@gmail.com>
2026-07-15 10:18:18 +0530

    Update media configuration URLs

--- ITIHASA/CLASS-3/content.js
+++ ITIHASA/CLASS-3/content.js
@@ -7,9 +7,9 @@ const APP_CONFIG = {
 const CLASS_NAME = "school9";
 
 const MEDIA_CONFIG = {
-    audio1: "https://audio.evidya.de/S9SSH1P1-2.mp3",
-    video:  "https://www.youtube.com/embed/JaW2P3OBDg4?playsinline=1&rel=0&modestbranding=1&autoplay=1",
-    pdf:    "https://audio.evidya.de/S9SSH1P1-2.pdf"
+    audio1: "https://audio.evidya.de/S9H2P3-4.mp3",
+    video:  "https://www.youtube.com/embed/rxPngCWZ0BY?playsinline=1&rel=0&modestbranding=1&autoplay=1",
+    pdf:    "https://audio.evidya.de/S9H2P3-4.pdf"
 };
 
 const wordData = {
