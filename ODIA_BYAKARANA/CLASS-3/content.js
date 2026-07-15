bf841b84ac6cfe6133752733e68f5f064a1d1ba2
happysangitanms <happysangitanms@gmail.com>
2026-07-15 11:27:57 +0530

    Update content.js

--- ODIA_BYAKARANA/CLASS-3/content.js
+++ ODIA_BYAKARANA/CLASS-3/content.js
@@ -8,7 +8,7 @@ const CLASS_NAME = "school9";
 
 const MEDIA_CONFIG = {
     audio1: "https://audio.evidya.de/S9FLOG1P2.mp3",
-    video:  "https://www.youtube.com/embed/2VF_yeUM7qs?playsinline=1&rel=0&modestbranding=1&autoplay=1",
+    video:  "https://www.youtube.com/embed/IpTuEBbLGmo?playsinline=1&rel=0&modestbranding=1&autoplay=1",
     pdf:    "https://audio.evidya.de/S9FLOG1P2.pdf"
 };
 
