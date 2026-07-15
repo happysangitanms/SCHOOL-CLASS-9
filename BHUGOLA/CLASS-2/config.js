8834bd5c6c8d30ddfd3b5feab2afca76113b41e1
happysangitanms <happysangitanms@gmail.com>
2026-07-16 01:04:39 +0530

    Create config.js

---
+++ BHUGOLA/CLASS-2/config.js
@@ -0,0 +1,13 @@
+const CLASS_MODE = "practice";
+
+const APP_CONFIG = {
+    sreni: "ନବମ",
+    subject: "ଭୂଗୋଳ",
+    classCode: "ଭୂଗୋଳ: ପ୍ରଥମ ଅଧ୍ୟାୟ (ପୃଷ୍ଠା 1-2)"
+};
+
+const CLASS_NAME = "school9";
+
+const MEDIA_CONFIG = {
+    summaryPdf: "https://audio.evidya.de/S9SSG1P1-2S.pdf"
+};
