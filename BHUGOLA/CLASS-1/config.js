25ebde3ab009849786f555f41dbfe4342863c416
happysangitanms <happysangitanms@gmail.com>
2026-07-16 00:49:49 +0530

    Create config.js

---
+++ BHUGOLA/CLASS-1/config.js
@@ -0,0 +1,12 @@
+const APP_CONFIG = {
+    sreni: "ନବମ",
+    subject: "ଭୂଗୋଳ"
+};
+
+const CLASS_NAME = "school9";
+
+const MEDIA_CONFIG = {
+    audio1: "https://audio.evidya.de/S9SSG1P1-2.mp3",
+    video:  "https://www.youtube.com/embed/qU8IfegijAo?playsinline=1&rel=0&modestbranding=1&autoplay=1",
+    pdf:    "https://audio.evidya.de/S9SSG1P1-2.pdf"
+};
