87a68309131f513bc6c3d4773a9d9a87dbd5ed8b
happysangitanms <happysangitanms@gmail.com>
2026-07-15 13:16:25 +0530

    Update content.js

--- SANSKRUTA_BYAKARAN/CLASS-3/content.js
+++ SANSKRUTA_BYAKARAN/CLASS-3/content.js
@@ -7,9 +7,9 @@ const APP_CONFIG = {
 const CLASS_NAME = "school9";
 
 const MEDIA_CONFIG = {
-    audio1: "https://audio.evidya.de/S9TLSG1P1-2.mp3",
+    audio1: "https://audio.evidya.de/S9TLSG1P2-3.mp3",
     video:  "https://www.youtube.com/embed/h7ZxhbTxWa8?playsinline=1&rel=0&modestbranding=1&autoplay=1",
-    pdf:    "https://audio.evidya.de/S9TLSG1P1-2.pdf"
+    pdf:    "https://audio.evidya.de/S9TLSG1P2-3.pdf"
 };
 
 const wordData = {
