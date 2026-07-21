d37844148a3182d471bfa0cb42f5c7484fdb2063
happysangitanms <happysangitanms@gmail.com>
2026-07-21 12:32:48 +0530

    Update fillblank.js

--- ENGLISH_BYAKARANA/CLASS-2/fillblank.js
+++ ENGLISH_BYAKARANA/CLASS-2/fillblank.js
@@ -1,1 +1,53 @@
+const fillBlanksData = {
 
+    questions: [
+
+        // =========================
+        //  CONCEPT NO.- 1
+        // =========================
+        {
+            question: "We express our ideas, feelings and views in ___ . (sentences / paragraphs) (ଆମେ ଆମର ଚିନ୍ତାଧାରା, ଅନୁଭବ ଓ ମତାମତ ___ ରେ ପ୍ରକାଶ କରୁ ।)",
+            answer: "sentences"
+        },
+
+        // =========================
+        // CONCEPT NO.- 2
+        // =========================
+        {
+            question: "The parts in column A are called ___ . (SUBJECTS / PREDICATES) (ସ୍ତମ୍ଭ A ର ଅଂଶଗୁଡ଼ିକୁ ___ କୁହାଯାଏ ।)",
+            answer: "SUBJECTS"
+        },
+
+        // =========================
+        // CONCEPT NO.- 3
+        // =========================
+        {
+            question: "The parts in column B are called ___ . (SUBJECTS / PREDICATES) (ସ୍ତମ୍ଭ B ର ଅଂଶଗୁଡ଼ିକୁ ___ କୁହାଯାଏ ।)",
+            answer: "PREDICATES"
+        },
+
+        // =========================
+        // CONCEPT NO.- 4
+        // =========================
+        {
+            question: "Words are arranged in a particular ___ to make a sentence. (order / colour) (ଏକ ବାକ୍ୟ ତିଆରି କରିବାକୁ ଶବ୍ଦଗୁଡ଼ିକୁ ଏକ ନିର୍ଦ୍ଦିଷ୍ଟ ___ ରେ ସଜାଯାଏ ।)",
+            answer: "order"
+        },
+
+        // =========================
+        // CONCEPT NO.- 5
+        // =========================
+        {
+            question: "Trees provide us with ___ . (oxygen / carbon dioxide) (ଗଛଗୁଡ଼ିକ ଆମକୁ ___ ଯୋଗାନ୍ତି ।)",
+            answer: "oxygen"
+        },
+
+        // =========================
+        // CONCEPT NO.- 6
+        // =========================
+        {
+            question: "Itishree's father, Mr Karunakar, is an ___ . (engineer / teacher) (ଇତିଶ୍ରୀଙ୍କ ପିତା, ଶ୍ରୀ କରୁଣାକର, ଜଣେ ___ ଅଟନ୍ତି ।)",
+            answer: "engineer"
+        }
+    ]
+},
