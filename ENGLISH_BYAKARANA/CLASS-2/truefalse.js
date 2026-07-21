27b0e35f4ceeb4d9422e505d79996af99e907fbc
happysangitanms <happysangitanms@gmail.com>
2026-07-21 12:46:36 +0530

    Update truefalse.js

--- ENGLISH_BYAKARANA/CLASS-2/truefalse.js
+++ ENGLISH_BYAKARANA/CLASS-2/truefalse.js
@@ -1,1 +1,69 @@
+const trueFalse: {
+    questions: [
+
+        // =========================
+        // CONCEPT NO.- 1
+        // =========================
+        {
+            question: "We express our ideas, feelings and views in sentences. (ଆମେ ଆମର ଚିନ୍ତାଧାରା, ଅନୁଭବ ଓ ମତାମତ ବାକ୍ୟରେ ପ୍ରକାଶ କରୁ ।)",
+            correct: true
+        },
+        // =========================
+        // CONCEPT NO.- 2
+        // =========================
+        {
+            question: "A sentence is made up of words and is a meaningful expression. (ଏକ ବାକ୍ୟ ଶବ୍ଦ ଦ୍ୱାରା ଗଠିତ ଏବଂ ଏହା ଏକ ଅର୍ଥପୂର୍ଣ୍ଣ ଅଭିବ୍ୟକ୍ତି ।)",
+            correct: true
+        },
+        // =========================
+        // CONCEPT NO.- 3
+        // =========================
+        {
+            question: "The parts in column A are called PREDICATES. (ସ୍ତମ୍ଭ A ର ଅଂଶଗୁଡ଼ିକୁ PREDICATE କୁହାଯାଏ ।)",
+            correct: false
+        },
+        // =========================
+        // CONCEPT NO.- 4
+        // =========================
+        {
+            question: "The parts in column B are called PREDICATES. (ସ୍ତମ୍ଭ B ର ଅଂଶଗୁଡ଼ିକୁ PREDICATE କୁହାଯାଏ ।)",
+            correct: true
+        },
+        // =========================
+        // CONCEPT NO.- 5
+        // =========================
+        {
+            question: "Words in a sentence can be arranged in any random order to make a meaningful sentence. (ଏକ ଅର୍ଥପୂର୍ଣ୍ଣ ବାକ୍ୟ ତିଆରି କରିବାକୁ ଶବ୍ଦଗୁଡ଼ିକୁ ଯେକୌଣସି କ୍ରମରେ ସଜାଯାଇପାରେ ।)",
+            correct: false
+        },
+        // =========================
+        // CONCEPT NO.- 6
+        // =========================
+        {
+            question: "'Indrani is a classical singer' is an acceptable sentence pattern. ('Indrani is a classical singer' ଏକ ଗ୍ରହଣୀୟ ବାକ୍ୟ ପାଟର୍ନ ଅଟେ ।)",
+            correct: true
+        },
+        // =========================
+        // CONCEPT NO.- 7
+        // =========================
+        {
+            question: "'Classical is Indrani a singer' is an acceptable sentence pattern. ('Classical is Indrani a singer' ଏକ ଗ୍ରହଣୀୟ ବାକ୍ୟ ପାଟର୍ନ ଅଟେ ।)",
+            correct: false
+        },
+        // =========================
+        // CONCEPT NO.- 8
+        // =========================
+        {
+            question: "Itishree is in class X. (ଇତିଶ୍ରୀ ଦଶମ ଶ୍ରେଣୀରେ ପଢ଼ୁଛନ୍ତି ।)",
+            correct: true
+        },
+        // =========================
+        // CONCEPT NO.- 9
+        // =========================
+        {
+            question: "Mr Karunakar, Itishree's father, is a doctor. (ଶ୍ରୀ କରୁଣାକର, ଇତିଶ୍ରୀଙ୍କ ପିତା, ଜଣେ ଡାକ୍ତର ଅଟନ୍ତି ।)",
+            correct: false
+        }
+    ]
+};
 
