874cd7283a1bcda80a6f34c4974eb1ffd6323338
happysangitanms <happysangitanms@gmail.com>
2026-07-21 17:30:59 +0530

    Update config.js

--- BHAUTIKA_BIGYANA/CLASS-3/config.js
+++ BHAUTIKA_BIGYANA/CLASS-3/config.js
@@ -1,1 +1,14 @@
+const APP_CONFIG = {
+    sreni: "ନବମ",
+    subject: "ଭୌତିକ ବିଜ୍ଞାନ"
+};
+
+
+const CLASS_NAME = "school9";
+
+const MEDIA_CONFIG = {
+    audio1: "https://audio.evidya.de/S9PS2P3-4.mp3",
+    video:  "https://www.youtube.com/embed/Y-ouPRcmGJ0?playsinline=1&rel=0&modestbranding=1&autoplay=1",
+    pdf:    "https://audio.evidya.de/S9PS2P3-4.pdf"
+};
 
