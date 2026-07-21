b7d52c856f9f47ed56d65541b496983d6bb098e0
happysangitanms <happysangitanms@gmail.com>
2026-07-21 12:24:38 +0530

    Update topicsdata.js

--- ENGLISH_BYAKARANA/CLASS-1/topicsdata.js
+++ ENGLISH_BYAKARANA/CLASS-1/topicsdata.js
@@ -1,1 +1,64 @@
 
+const topicsData = [
+
+{
+heading: "NOTE-1: ବାକ୍ୟ, କର୍ତ୍ତା ଓ ପ୍ରେଡିକେଟ୍ କ'ଣ ? :-",
+
+audioPoints: [{
+text:"👉 ଧାରଣା: ଆମେ ଆମର ଭାବ, ଅନୁଭୂତି ଓ ମତାମତ ବାକ୍ୟ ମାଧ୍ୟମରେ ପ୍ରକାଶ କରୁ । ବାକ୍ୟ ଶବ୍ଦରେ ଗଠିତ ହୋଇଥାଏ ଏବଂ ଏହା ଏକ ଅର୍ଥପୂର୍ଣ୍ଣ ପ୍ରକାଶ ଅଟେ । ପ୍ରତ୍ୟେକ ବାକ୍ୟର ଦୁଇଟି ମୁଖ୍ୟ ଅଂଶ ଥାଏ - କର୍ତ୍ତା (Subject) ଓ ପ୍ରେଡିକେଟ୍ (Predicate) । କର୍ତ୍ତା ହେଉଛି ଯାହା ବିଷୟରେ କୁହାଯାଏ, ପ୍ରେଡିକେଟ୍ ହେଉଛି ତାହା ବିଷୟରେ କ'ଣ କୁହାଯାଏ ।<br><br>👉 ଉଦାହରଣ: 'That boy is intelligent' ବାକ୍ୟରେ 'That boy' କର୍ତ୍ତା ଏବଂ 'is intelligent' ପ୍ରେଡିକେଟ୍ ଅଟେ । ସେହିପରି 'Trees provide us with oxygen' ରେ 'Trees' କର୍ତ୍ତା ଏବଂ 'provide us with oxygen' ପ୍ରେଡିକେଟ୍ ଅଟେ ।",
+audio:"audio-url-here"}],
+
+questions:[
+
+"<span style='color:red;'>Q-1:</span> ବାକ୍ୟ କାହାକୁ କୁହାଯାଏ ?<br><br><span style='color:green;'>Answer:</span> ଶବ୍ଦରେ ଗଠିତ ହୋଇ ଅର୍ଥପୂର୍ଣ୍ଣ ପ୍ରକାଶ କରୁଥିବା ବିଷୟକୁ ବାକ୍ୟ କୁହାଯାଏ ।",
+
+"<span style='color:red;'>Q-2:</span> ପ୍ରତ୍ୟେକ ବାକ୍ୟର ମୁଖ୍ୟ ଅଂଶଗୁଡ଼ିକର ନାମ କ'ଣ ?<br><br><span style='color:green;'>Answer:</span> କର୍ତ୍ତା (Subject) ଓ ପ୍ରେଡିକେଟ୍ (Predicate) ।",
+
+"<span style='color:red;'>Q-3:</span> କର୍ତ୍ତା କ'ଣ ବୁଝାଏ ?<br><br><span style='color:green;'>Answer:</span> ଯାହା ବିଷୟରେ ବାକ୍ୟରେ କୁହାଯାଏ, ତାହାକୁ କର୍ତ୍ତା କୁହାଯାଏ ।",
+
+"<span style='color:red;'>Q-4:</span> 'Trees provide us with oxygen' ବାକ୍ୟରେ ପ୍ରେଡିକେଟ୍ କେଉଁଟି ?<br><br><span style='color:green;'>Answer:</span> 'provide us with oxygen' ଏହି ବାକ୍ୟର ପ୍ରେଡିକେଟ୍ ଅଟେ ।"
+
+]
+},
+
+{
+heading: "NOTE-2: ଶବ୍ଦକ୍ରମର ଗୁରୁତ୍ୱ :-",
+
+audioPoints: [{
+text:"👉 ଧାରଣା: ବାକ୍ୟ ତିଆରି କରିବା ବେଳେ ଶବ୍ଦଗୁଡ଼ିକୁ ଏକ ନିର୍ଦ୍ଦିଷ୍ଟ କ୍ରମରେ ସଜାଇବାକୁ ପଡ଼େ । ଶବ୍ଦଗୁଡ଼ିକ ଠିକ୍ ଥିଲେ ମଧ୍ୟ, ସେଗୁଡ଼ିକୁ ଭୁଲ କ୍ରମରେ ସଜାଇଲେ ବାକ୍ୟର ଅର୍ଥ ବୁଝି ହୁଏ ନାହିଁ । ଏଣୁ ସଠିକ୍ ଶବ୍ଦକ୍ରମ ବ୍ୟବହାର କରିବା ଅତ୍ୟନ୍ତ ଜରୁରୀ ।<br><br>👉 ଉଦାହରଣ: 'Indrani is a classical singer' ଏକ ସଠିକ୍ ବାକ୍ୟ, କିନ୍ତୁ 'Singer classical Indrani is a' କୌଣସି ଅର୍ଥ ଦିଏ ନାହିଁ । ସେହିପରି 'her / you / waiting / for / are' ଶବ୍ଦଗୁଡ଼ିକୁ ସଜାଇଲେ 'Are you waiting for her ?' ଏକ ସଠିକ୍ ପ୍ରଶ୍ନ ବାକ୍ୟ ହୁଏ ।",
+audio:"audio-url-here"}],
+
+questions:[
+
+"<span style='color:red;'>Q-1:</span> ବାକ୍ୟ ତିଆରି କରିବା ବେଳେ ଶବ୍ଦଗୁଡ଼ିକୁ କେମିତି ସଜାଇବାକୁ ପଡ଼େ ?<br><br><span style='color:green;'>Answer:</span> ଶବ୍ଦଗୁଡ଼ିକୁ ଏକ ନିର୍ଦ୍ଦିଷ୍ଟ କ୍ରମରେ ସଜାଇବାକୁ ପଡ଼େ ।",
+
+"<span style='color:red;'>Q-2:</span> ଭୁଲ ଶବ୍ଦକ୍ରମରେ ବାକ୍ୟ ଲେଖିଲେ କ'ଣ ହୁଏ ?<br><br><span style='color:green;'>Answer:</span> ବାକ୍ୟର ଅର୍ଥ ବୁଝି ହୁଏ ନାହିଁ ।",
+
+"<span style='color:red;'>Q-3:</span> 'her / you / waiting / for / are' ଶବ୍ଦଗୁଡ଼ିକୁ ସଜାଇଲେ କେଉଁ ବାକ୍ୟ ତିଆରି ହୁଏ ?<br><br><span style='color:green;'>Answer:</span> 'Are you waiting for her ?' ବାକ୍ୟ ତିଆରି ହୁଏ ।",
+
+"<span style='color:red;'>Q-4:</span> ସଠିକ୍ ଶବ୍ଦକ୍ରମର ଉଦାହରଣ କ'ଣ ?<br><br><span style='color:green;'>Answer:</span> 'Indrani is a classical singer' ଏକ ସଠିକ୍ ଶବ୍ଦକ୍ରମର ଉଦାହରଣ ଅଟେ ।"
+
+]
+},
+
+{
+heading: "NOTE-3: ସରଳ ବାକ୍ୟ ମାଧ୍ୟମରେ ବର୍ଣ୍ଣନା :-",
+
+audioPoints: [{
+text:"👉 ଧାରଣା: ଆମେ ଛୋଟ ଓ ସରଳ ବାକ୍ୟ ମାଧ୍ୟମରେ ଜଣଙ୍କ ବିଷୟରେ ପୂରା ସୂଚନା ଦେଇପାରିବା । ପ୍ରତ୍ୟେକ ବାକ୍ୟ ଗୋଟିଏ ଗୋଟିଏ ତଥ୍ୟ କୁହେ, ଏବଂ ପ୍ରତ୍ୟେକ ବାକ୍ୟରେ କର୍ତ୍ତା ଓ ପ୍ରେଡିକେଟ୍ ଥାଏ ।<br><br>👉 ଉଦାହରଣ: 'This is Itishree.' 'She is in class X.' 'She is fourteen.' 'Mr Karunakar is her father.' 'He is an engineer.' ଏହିପରି ପ୍ରତ୍ୟେକ ସରଳ ବାକ୍ୟ ଇତିଶ୍ରୀ ବିଷୟରେ ଗୋଟିଏ ନୂଆ ତଥ୍ୟ ଦିଏ ।",
+audio:"audio-url-here"}],
+
+questions:[
+
+"<span style='color:red;'>Q-1:</span> ସରଳ ବାକ୍ୟ ଆମକୁ କ'ଣ ଦେଇଥାଏ ?<br><br><span style='color:green;'>Answer:</span> ସରଳ ବାକ୍ୟ ଆମକୁ ଜଣଙ୍କ ବିଷୟରେ ଗୋଟିଏ ଗୋଟିଏ ସ୍ପଷ୍ଟ ତଥ୍ୟ ଦେଇଥାଏ ।",
+
+"<span style='color:red;'>Q-2:</span> 'She is in class X' ବାକ୍ୟଟି କାହା ବିଷୟରେ ଥିଲା ?<br><br><span style='color:green;'>Answer:</span> ଏହା ଇତିଶ୍ରୀ ବିଷୟରେ ଥିଲା ।",
+
+"<span style='color:red;'>Q-3:</span> ଇତିଶ୍ରୀଙ୍କ ପିତାଙ୍କ ବୃତ୍ତି କ'ଣ ?<br><br><span style='color:green;'>Answer:</span> ତାଙ୍କ ପିତା ଜଣେ ଇଞ୍ଜିନିୟର ।",
+
+"<span style='color:red;'>Q-4:</span> ଜଣଙ୍କ ବିଷୟରେ ପୂରା ସୂଚନା ଦେବାକୁ ଆମେ କେମିତି ବାକ୍ୟ ବ୍ୟବହାର କରୁ ?<br><br><span style='color:green;'>Answer:</span> ଆମେ ଛୋଟ ଓ ସରଳ ବାକ୍ୟ ବ୍ୟବହାର କରୁ ।"
+
+]
+}
+
+];
