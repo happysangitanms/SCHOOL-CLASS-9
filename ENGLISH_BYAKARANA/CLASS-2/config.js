6a5e88ee4cc276c8cbb36ff318f9ee2152153bb6
happysangitanms <happysangitanms@gmail.com>
2026-07-21 12:29:59 +0530

    Add configuration for class and media settings

--- ENGLISH_BYAKARANA/CLASS-2/config.js
+++ ENGLISH_BYAKARANA/CLASS-2/config.js
@@ -1,1 +1,15 @@
+const CLASS_MODE = "practice";
+
+const APP_CONFIG = {
+    sreni: "ନବମ",
+    subject: "ଇଂରାଜୀ ବ୍ଯାକରଣ",
+    classCode: "ଇଂରାଜୀ ବ୍ଯାକରଣ: ପ୍ରଥମ ଅଧ୍ୟାୟ (ପୃଷ୍ଠା 1-2)"
+};
+
+
+const CLASS_NAME = "school9";
+
+const MEDIA_CONFIG = {
+    summaryPdf: "https://audio.evidya.de/S9SLEG1P1-2S.pdf"
+};
 
