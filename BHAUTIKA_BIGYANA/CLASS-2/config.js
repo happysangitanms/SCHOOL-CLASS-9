9c45df33f15434cc6bd22c4ad774db7ce016757a
happysangitanms <happysangitanms@gmail.com>
2026-07-21 17:21:51 +0530

    Update config.js

--- BHAUTIKA_BIGYANA/CLASS-2/config.js
+++ BHAUTIKA_BIGYANA/CLASS-2/config.js
@@ -1,1 +1,14 @@
+const CLASS_MODE = "practice";
 
+const APP_CONFIG = {
+    sreni: "ନବମ",
+    subject: "ଭୌତିକ ବିଜ୍ଞାନ",
+    classCode: "ଭୌତିକ ବିଜ୍ଞାନ: ପ୍ରଥମ ଅଧ୍ୟାୟ (ପୃଷ୍ଠା 1-2)"
+};
+
+
+const CLASS_NAME = "school9";
+
+const MEDIA_CONFIG = {
+    summaryPdf: "https://audio.evidya.de/S9SCP1P1-2S.pdf"
+};
