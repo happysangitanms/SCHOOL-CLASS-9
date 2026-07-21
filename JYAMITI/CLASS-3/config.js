f77f71f0372bc383edac017b065cc12924dc605e
happysangitanms <happysangitanms@gmail.com>
2026-07-21 15:06:02 +0530

    Update config.js

--- JYAMITI/CLASS-3/config.js
+++ JYAMITI/CLASS-3/config.js
@@ -1,1 +1,14 @@
+const APP_CONFIG = {
+    sreni: "ନବମ",
+    subject: "ଜ୍ୟାମିତି"
+};
+
+
+const CLASS_NAME = "school9";
+
+const MEDIA_CONFIG = {
+    audio1: "https://audio.evidya.de/S9MTG2P2-4.mp3",
+    video:  "https://www.youtube.com/embed/35h4o8ElesU?playsinline=1&rel=0&modestbranding=1&autoplay=1",
+    pdf:    "https://audio.evidya.de/S9MTG2P2-4.pdf"
+};
 
