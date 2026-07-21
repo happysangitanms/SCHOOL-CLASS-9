7c289bcfa91ae9d2e3fe30dcec103e81851848ca
happysangitanms <happysangitanms@gmail.com>
2026-07-21 13:02:24 +0530

    Update config.js

--- ITIHASA/CLASS-1/config.js
+++ ITIHASA/CLASS-1/config.js
@@ -1,1 +1,13 @@
+const APP_CONFIG = {
+    sreni: "ନବମ",
+    subject: "ଇତିହାସ "
+};
 
+
+const CLASS_NAME = "school9";
+
+const MEDIA_CONFIG = {
+    audio1: "https://audio.evidya.de/S9SSH1P1-2.mp3",
+    video:  "https://www.youtube.com/embed/JaW2P3OBDg4?playsinline=1&rel=0&modestbranding=1&autoplay=1",
+    pdf:    "https://audio.evidya.de/S9SSH1P1-2.pdf"
+};
