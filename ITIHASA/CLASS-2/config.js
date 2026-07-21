1b2e88f72cc38d756499be816d4f8c99f4355acb
happysangitanms <happysangitanms@gmail.com>
2026-07-21 13:21:36 +0530

    Update config.js

--- ITIHASA/CLASS-2/config.js
+++ ITIHASA/CLASS-2/config.js
@@ -1,1 +1,14 @@
+const CLASS_MODE = "practice";
 
+const APP_CONFIG = {
+    sreni: "ନବମ",
+    subject: "ଇତିହାସ",
+    classCode: "ଇତିହାସ: ପ୍ରଥମ ଅଧ୍ୟାୟ (ପୃଷ୍ଠା 1-2)"
+};
+
+
+const CLASS_NAME = "school9";
+
+const MEDIA_CONFIG = {
+    summaryPdf: "https://audio.evidya.de/S9SSH1P1-2S.pdf"
+};
