9be9ddd80196cd379b08fda97437094ea761ac36
happysangitanms <happysangitanms@gmail.com>
2026-07-21 13:53:45 +0530

    Update config.js

--- JIBA_BIGYANA/CLASS-3/config.js
+++ JIBA_BIGYANA/CLASS-3/config.js
@@ -1,1 +1,10 @@
+const CLASS_MODE = "practice";
 
+const APP_CONFIG = {
+    sreni: "ନବମ",
+    subject: "ଜୀବ ବିଜ୍ଞାନ",
+    classCode: "ଜୀବ ବିଜ୍ଞାନ: ପ୍ରଥମ ଅଧ୍ୟାୟ (ପୃଷ୍ଠା 1-2)"
+};
+
+
+const CLASS_NAME = "school9";
