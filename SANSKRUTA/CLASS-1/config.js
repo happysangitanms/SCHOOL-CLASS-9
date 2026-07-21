f92076f5f20f3fb959ebee3205d45946252e133f
happysangitanms <happysangitanms@gmail.com>
2026-07-21 16:06:56 +0530

    Update config.js

--- SANSKRUTA/CLASS-1/config.js
+++ SANSKRUTA/CLASS-1/config.js
@@ -1,1 +1,13 @@
+const APP_CONFIG = {
+    sreni: "ନବମ",
+    subject: "ସଂସ୍କୃତ"
+};
 
+
+const CLASS_NAME = "school9";
+
+const MEDIA_CONFIG = {
+    audio1: "https://audio.evidya.de/S9TLS1P2-3.mp3",
+    video:  "https://www.youtube.com/embed/QS0X7cpzo1g?playsinline=1&rel=0&modestbranding=1&autoplay=1",
+    pdf:    "https://audio.evidya.de/S9TLS1P2-3.pdf"
+};
