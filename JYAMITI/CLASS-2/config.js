1457dd5112573c20e10dedce887f607c4afa6b23
happysangitanms <happysangitanms@gmail.com>
2026-07-21 14:22:00 +0530

    Update config.js

--- JYAMITI/CLASS-2/config.js
+++ JYAMITI/CLASS-2/config.js
@@ -1,1 +1,14 @@
+const CLASS_MODE = "practice";
 
+const APP_CONFIG = {
+    sreni: "ନବମ",
+    subject: "ଜ୍ୟାମିତି",
+    classCode: "ଜ୍ୟାମିତି: ପ୍ରଥମ ଅଧ୍ୟାୟ (ପୃଷ୍ଠା 1-2)"
+};
+
+
+const CLASS_NAME = "school9";
+
+const MEDIA_CONFIG = {
+    summaryPdf: "https://audio.evidya.de/S9MTG1P1-2S.pdf"
+};
