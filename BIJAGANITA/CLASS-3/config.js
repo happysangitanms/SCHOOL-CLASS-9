10f75c481fcb607e1df8a69755f98a079c2e0077
happysangitanms <happysangitanms@gmail.com>
2026-07-21 17:16:14 +0530

    Update config.js

--- BIJAGANITA/CLASS-3/config.js
+++ BIJAGANITA/CLASS-3/config.js
@@ -1,1 +1,13 @@
+const APP_CONFIG = {
+    sreni: "ନବମ",
+    subject: "ବୀଜଗଣିତ"
+};
 
+
+const CLASS_NAME = "school9";
+
+const MEDIA_CONFIG = {
+    audio1: "https://audio.evidya.de/S9MTA2P2-8.mp3",
+    video:  "https://www.youtube.com/embed/p8OXEktMUfM?playsinline=1&rel=0&modestbranding=1&autoplay=1",
+    pdf:    "https://audio.evidya.de/S9MTA2P2-8.pdf"
+};
