7532f86f496d08c33b555a1b228c15c3e5866049
happysangitanms <happysangitanms@gmail.com>
2026-07-24 01:52:25 +0530

    Update config.js

--- SCP/CLASS-1/config.js
+++ SCP/CLASS-1/config.js
@@ -2,7 +2,7 @@ const APP_CONFIG = {
     sreni: "ନବମ",
     subject: "ଭୌତିକ ବିଜ୍ଞାନ"
 };
-const CLASS_NAME = "1";
+const CLASS_NAME = "school9";
 const MEDIA_CONFIG = {
     audio1: "https://audio.evidya.de/CLASS-1SCP1.mp3",
     video:  "https://www.youtube.com/embed/kAEsYKlS7eY?playsinline=1&rel=0&modestbranding=1&autoplay=1",
