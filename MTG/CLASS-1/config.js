d3eb410138ef3147b4706f5682b19b5ab1138efe
happysangitanms <happysangitanms@gmail.com>
2026-07-23 20:11:02 +0530

    Update config.js

--- MTG/CLASS-1/config.js
+++ MTG/CLASS-1/config.js
@@ -2,7 +2,7 @@ const APP_CONFIG = {
     sreni: "ନବମ",
     subject: "ଜ୍ୟାମିତି"
 };
-const CLASS_NAME = "1";
+const CLASS_NAME = "school9";
 const MEDIA_CONFIG = {
     audio1: "https://audio.evidya.de/CLASS-1MTG1.mp3",
     video:  "https://www.youtube.com/embed/cdYXIZ5IGPQ?playsinline=1&rel=0&modestbranding=1&autoplay=1",
