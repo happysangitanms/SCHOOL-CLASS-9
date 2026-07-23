d49954df2a4787c7644678ace63140d6fe236e05
happysangitanms <happysangitanms@gmail.com>
2026-07-23 23:45:13 +0530

    Update config.js

--- ODIA_BYAKARANA/CLASS-3/config.js
+++ ODIA_BYAKARANA/CLASS-3/config.js
@@ -1,1 +1,13 @@
+const APP_CONFIG = {
+    sreni: "ନବମ",
+    subject: "ଓଡ଼ିଆ ବ୍ୟାକରଣ"
+};
 
+
+const CLASS_NAME = "school9";
+
+const MEDIA_CONFIG = {
+    audio1: "https://audio.evidya.de/S9FLOG2P2.mp3",
+    video:  "https://www.youtube.com/embed/IpTuEBbLGmo?playsinline=1&rel=0&modestbranding=1&autoplay=1",
+    pdf:    "https://audio.evidya.de/S9FLOG2P2.pdf"
+};
