69110a11733eabbd463d92c1a12635f51c2c5eaf
happysangitanms <happysangitanms@gmail.com>
2026-07-23 20:10:14 +0530

    Update config.js

--- MTA/CLASS-1/config.js
+++ MTA/CLASS-1/config.js
@@ -2,7 +2,7 @@ const APP_CONFIG = {
     sreni: "ନବମ",
     subject: "ବୀଜଗଣିତ"
 };
-const CLASS_NAME = "1";
+const CLASS_NAME = "school9";
 const MEDIA_CONFIG = {
     audio1: "https://audio.evidya.de/CLASS-1MTA1.mp3",
     video:  "https://www.youtube.com/embed/6rxjRZ8-ZfY?playsinline=1&rel=0&modestbranding=1&autoplay=1",
