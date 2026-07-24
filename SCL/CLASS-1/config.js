630d6314d369c04ae261cf19e0ad20486440330b
happysangitanms <happysangitanms@gmail.com>
2026-07-24 21:06:22 +0530

    Publish config.js with video ID for SMR-28

---
+++ SCL/CLASS-1/config.js
@@ -0,0 +1,10 @@
+const APP_CONFIG = {
+    sreni: "ନବମ",
+    subject: "ଜୀବ ବିଜ୍ଞାନ"
+};
+const CLASS_NAME = "school9";
+const MEDIA_CONFIG = {
+    audio1: "https://audio.evidya.de/9SCL1.mp3",
+    video:  "https://www.youtube.com/embed/q_MwZmhhef8?playsinline=1&rel=0&modestbranding=1&autoplay=1",
+    pdf:    "https://audio.evidya.de/9SCL1.pdf"
+};
