86ba7c5088e049f1f78e2314fd89f69caf598b48
happysangitanms <happysangitanms@gmail.com>
2026-07-24 19:56:57 +0530

    Publish config.js with video ID for SMR-21

---
+++ SSH/CLASS-1/config.js
@@ -0,0 +1,10 @@
+const APP_CONFIG = {
+    sreni: "ନବମ",
+    subject: "ଇତିହାସ"
+};
+const CLASS_NAME = "school9";
+const MEDIA_CONFIG = {
+    audio1: "https://audio.evidya.de/CLASS-1SSH1.mp3",
+    video:  "https://www.youtube.com/embed/Yd_26tk1iyo?playsinline=1&rel=0&modestbranding=1&autoplay=1",
+    pdf:    "https://audio.evidya.de/CLASS-1SSH1.pdf"
+};
