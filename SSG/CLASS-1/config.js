628842c5dc4ab0187c78f38f96fd572d7f50d9da
happysangitanms <happysangitanms@gmail.com>
2026-07-24 21:53:38 +0530

    Publish config.js with video ID for SMR-35

---
+++ SSG/CLASS-1/config.js
@@ -0,0 +1,10 @@
+const APP_CONFIG = {
+    sreni: "ନବମ",
+    subject: "ଭୂଗୋଳ"
+};
+const CLASS_NAME = "school9";
+const MEDIA_CONFIG = {
+    audio1: "https://audio.evidya.de/9SSG1.mp3",
+    video:  "https://www.youtube.com/embed/ikhd-mm-ilA?playsinline=1&rel=0&modestbranding=1&autoplay=1",
+    pdf:    "https://audio.evidya.de/9SSG1.pdf"
+};
